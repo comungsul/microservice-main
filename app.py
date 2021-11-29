4674e0182c3a041e7dcb5387361dbadde759263b
Sean Hong <seanhong2000@gmail.com>
2021-11-30 02:05:42 +0900

    Add temporary Jinja templating

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
 from flask import render_template
+from flask_cors import CORS
+from src.api import get_sports, get_personal_blog
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    json_sports = get_sports()
+    json_personal_blog = get_personal_blog()
+    return render_template('index.html',
+        content_sports=json_sports,
+        content_blog=json_personal_blog)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
